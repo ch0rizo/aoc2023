da4abc3f1aba93bee78cfb8274d2ac22e5ef245c
Christoffer Nguyen <christofferhoang@gmail.com>
2024-12-22 00:42:59 +0100

    next

---
+++ day-1/day-01-2-solve.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+fs.readFile('day-01-input.txt', (err, data) => {
+  if (err) throw err;
+
+  const input = data.toString().split('\n');
+  const final = [];
+
+  function eraseChar(array) {
+    const regex = /[a-z]+/gi;
+    for (i = 0; i < array.length; i++) {
+      let result = array[i].replace(regex, '');
+      let semi = [];
+      if (result.length > 1) {
+        semi.push(result[0] + result[result.length - 1]);
+      } else {
+        semi.push(result[0] + result[0]);
+      }
+      final.push(parseInt(semi));
+    }
+  }
+
+  eraseChar(input);
+  console.log(final);
+  let sum = final.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+
+  console.log(sum);
